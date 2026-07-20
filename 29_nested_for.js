c9067590e32f9000f3eb604b9d6daa90ebcfa0c1
CodingModi <vishwakanya28@gmail.com>
2026-07-20 11:11:58 +0530

    Add JavaScript files for conditional statements, loops, and nested loops

---
+++ 29_nested_for.js
@@ -0,0 +1,8 @@
+let i = 1;
+for(let i = 1; i <= 3; i++) {
+         for(let j = 1; j <= 2; j++){
+                 console.log("i = " + i + ", j = " + j); 
+                 
+                } 
+            } 
+        
